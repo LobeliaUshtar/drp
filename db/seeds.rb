a58803de0a09dc6e5875eb84fae96e28ca8f198f
Becky Osiecki <lobeliaushtar@gmail.com>
2014-08-04 15:13:55 -0400

    basic layout, skills up, 2 admins set

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+User.create!([
+  {
+    username: "gabby",
+    password: "password",
+    password_confirmation: "password",
+    admin: true
+  },
+  {
+    username: "elisa",
+    password: "password",
+    password_confirmation: "password",
+    admin: true
+  }
+])
