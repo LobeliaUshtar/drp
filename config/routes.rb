125fa1be11e5d3652376a2f55343971ba70a9367
Becky Osiecki <lobeliaushtar@gmail.com>
2014-08-09 12:06:28 -0400

    session login + some layout fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'skills#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   
   resources :users
   resources :skills
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,39 @@ User.create!([
     admin: true
   }
 ])
+
+Skill.create!([
+  {
+    name: "Expert Prep/Brush & Roll Craftsmen"
+  },
+  {
+    name: "Carpentry, Repairs, Water Damage"
+  },
+  {
+    name: "Sheetrocking, Plastering, Masonry"
+  },
+  {
+    name: "Staining, Epoxy, Stucco, Concrete"
+  },
+  {
+    name: "Kitchen/Bath Cabinet Custom Painting"
+  },
+  {
+    name: "Restorations, Refinishing, Paper Removal"
+  },
+  {
+    name: "Paint Stripping, Peeling, Power Sanding"
+  },
+  {
+    name: "Lead Abatement Specialists"
+  },
+  {
+    name: "Aluminum Siding, Decks, Fences"
+  },
+  {
+    name: "Power Washing/Gutters Cleaned"
+  },
+  {
+    name: "Historical Painting"
+  }
+])
